950f64516bf61976ed42c025ff57cbb279a5179f
pujazon <danielpujazonbonet@gmail.com>
2019-01-25 17:40:02 +0000

    First GPIO WiringPi program and cleaning

--- Camera/src/blink.c
+++ Camera/src/blink.c
@@ -1,14 +1,20 @@
 #include <wiringPi.h>
+#include <stdio.h>
+
+//Wiring pi id's https://es.pinout.xyz/pinout/
 
 int main(void){
 
 	wiringPiSetup();
 	pinMode (0,OUTPUT);
 	
-	for(;;){
+	while(1){
+		
+		printf("Blink..\n");
 		
 		digitalWrite(0,HIGH);
 		delay(500);
+		
 		digitalWrite(0,LOW);
 		delay(500);
 	}
